5c7085775d04c45e82301c3b2d4de11151dd2552
AdminFedora <segor6052@gmail.com>
2025-03-25 12:45:27 +0200

    Modify future auction

--- SothbeysKillerApi/Controllers/AuctionController.cs
+++ SothbeysKillerApi/Controllers/AuctionController.cs
@@ -10,8 +10,7 @@ public record AuctionUpdateRequest(DateTime Start, DateTime Finish);
 
 public record AuctionResponse(Guid Id, string Title, DateTime Start, DateTime Finish);
 
-public class Auction
-{
+public class Auction {
     public Guid Id { get; set; }
     public string Title { get; set; }
     public DateTime Start { get; set; }
@@ -34,7 +33,6 @@ public class AuctionController : ControllerBase
     public IActionResult Past()
     {
         var auctions = _auctionService.GetPastAuctions();
-        
         return Ok(auctions);
     }
     
@@ -43,7 +41,7 @@ public class AuctionController : ControllerBase
     public IActionResult Active()
     {
         var auctions = _auctionService.GetActiveAuctions();
-        
+        // code 200
         return Ok(auctions);
     }
     
@@ -52,7 +50,7 @@ public class AuctionController : ControllerBase
     public IActionResult Future()
     {
         var auctions = _auctionService.GetFutureAuctions();
-        
+        // code 200
         return Ok(auctions);
     }
 
@@ -62,11 +60,11 @@ public class AuctionController : ControllerBase
         try
         {
             var id = _auctionService.CreateAuction(request);
-            
             return Ok(new { Id = id });
         }
         catch (ArgumentException)
         {
+            // code 400
             return BadRequest();
         }
     }
@@ -77,30 +75,38 @@ public class AuctionController : ControllerBase
         try
         {
             var auction = _auctionService.GetAuctionById(id);
-
             return Ok(auction);
         }
         catch (NullReferenceException)
         {
+            // code 404
             return NotFound();
         }
     }
 
     [HttpPut("{id:guid}")]
-    public IActionResult Update(Guid id, AuctionUpdateRequest request)
+    public IActionResult Update(Guid id, [FromBody] AuctionUpdateRequest request)
     {
+        if (request == null || request.Start >= request.Finish)
+        {
+            // code 400
+            return BadRequest();
+        }
+
         try
         {
             _auctionService.UpdateAuction(id, request);
-
+            // code 204
             return NoContent();
         }
         catch (NullReferenceException)
         {
+            // code 404
             return NotFound();
         }
         catch (ArgumentException)
         {
+            // code 400
             return BadRequest();
         }
     }
@@ -111,15 +117,17 @@ public class AuctionController : ControllerBase
         try
         {
             _auctionService.DeleteAuction(id);
-
+            // code 204
             return NoContent();
         }
         catch (NullReferenceException)
         {
+            // code 404
             return NotFound();
         }
         catch (ArgumentException)
         {
+            // code 400
             return BadRequest();
         }
     }
